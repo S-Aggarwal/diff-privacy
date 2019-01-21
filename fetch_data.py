d94aa3c7ba53314af0b5e64238ccd2c7a1b29440
Shubham Aggarwal <sa827@cam.ac.uk>
2019-01-21 07:07:57 +0000

    Moved a lot of the code to the notebook as native isn't working for now

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,5 @@
-# import osmnx as ox
+import matplotlib
+import osmnx as ox
 import pandas as pd
 import os
 import urllib.request
@@ -10,9 +11,9 @@ class TaxiDataset:
 		self.coords = coords
 		self.timestamps = timestamps
 	
-def fetch_dataset(date='2016-01', location='manhattan'):
+def fetch_taxi_dataset(location='manhattan', date='2016-01'):
 	"""
-	Downloads and caches the NYC dataset for the given date.
+	Downloads and caches the taxi dataset for the given location (and date for NYC).
 	"""
 	filename = 'yellow_tripdata_' + date + '.csv'
 	filepath = 'data/' + location + '/' + filename
@@ -22,3 +23,14 @@ def fetch_dataset(date='2016-01', location='manhattan'):
 	
 	df = pd.read_csv(filepath, nrows=10, usecols=['tpep_pickup_datetime', 'pickup_longitude', 'pickup_latitude'])
 	return TaxiDataset(location, zip(df['pickup_longitude'], df['pickup_latitude']), df['tpep_pickup_datetime'])
+
+
+def fetch_osm_map(location="manhattan"):
+	"""
+	Downloads and caches the OSM street graph for the given location
+	"""
+
+	raw_graph = ox.graph_from_place("Manhattan, New York, USA", network_type='drive')
+	ox.plot_graph(raw_graph)
+
+fetch_osm_map()
